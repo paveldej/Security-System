8c4a3f703ed746f38dbf459751bd150334f25960
Mate_Stier <mates@chalmers.se>
2025-04-15 18:14:27 +0200

    Add pin header file
    This ways it's easier to change and read

--- Wio Terminal Code/AlarmTrigger.ino
+++ Wio Terminal Code/AlarmTrigger.ino
@@ -3,46 +3,46 @@
 #include "AlarmTrigger.h"
 #include <ChainableLED.h>
 #include "Ultrasonic.h"
+#include "pins.h"
 
-//hard-coded ports for now, might change it later
-ChainableLED leds(2, 3, 1);
-Ultrasonic ultrasonicSensor(D0);
 
-void AlarmTrigger::turnLightRed()
-{
+ChainableLED leds(RGB_LED_DI_PIN, RGB_LED_CI_PIN, 1);
+Ultrasonic ultrasonicSensor(ULTRASONIC_PIN);
+
+void AlarmTrigger::turnLightRed() {
   leds.setColorRGB(0, 50, 0, 0);
 }
 
-void AlarmTrigger::turnLightGreen()
-{
+void AlarmTrigger::turnLightGreen() {
   leds.setColorRGB(0, 0, 50, 0);
 }
 
 //utilzes both the in-built and connected (port:D04) buzzers
-void AlarmTrigger::playAlarmSound()
-{
+void AlarmTrigger::playAlarmSound() {
   pinMode(WIO_BUZZER, OUTPUT);
   analogWrite(WIO_BUZZER, 64);
-  digitalWrite(4, HIGH);
+  digitalWrite(BUZZER_PIN, HIGH);
 }
 
-void AlarmTrigger::stopAlarmSound(){
+void AlarmTrigger::stopAlarmSound() {
   analogWrite(WIO_BUZZER, 0);
-  digitalWrite(4, LOW);
+  digitalWrite(BUZZER_PIN, LOW);
 }
 
-bool AlarmTrigger::objectIsClose(long threshold)
-{
+bool AlarmTrigger::objectIsClose(long threshold) {
+
   long distance = ultrasonicSensor.MeasureInCentimeters();
+
   if (distance <= threshold){
     return true;
+
   } else {
     return false;
   }
 }
 
 //the seconds denote how long should the alarm be triggered
-void AlarmTrigger::triggerAlarm(int seconds){
+void AlarmTrigger::triggerAlarm(int seconds) {
 
   alarmTrigger.playAlarmSound();
   alarmTrigger.turnLightRed();
