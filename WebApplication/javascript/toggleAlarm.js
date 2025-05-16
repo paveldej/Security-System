d99f21f10b34be7d688b788ba57210a86531ff2c
Riad <santir@student.chalmers.se>
2025-05-16 18:09:26 +0200

    #41 redesign the UI with improvements regarding layout and styles

--- WebApplication/javascript/toggleAlarm.js
+++ WebApplication/javascript/toggleAlarm.js
@@ -19,9 +19,9 @@ client.on('connect', () => {
 client.on('message', function (topic, message) {
   if (topic === "Status/getStatus") {
     s = message.toString();
+    updateUI()
     console.log('Received status:', s);
     console.log(s);
-    updateUI();
   }
 });
 
@@ -29,19 +29,34 @@ client.on('message', function (topic, message) {
 function updateUI() {
   if (s === "disarm") {
     armButton.textContent = "Arm";
-    systemStatus.textContent = "Status: Disarmed";
+    systemStatus.textContent = "Disarmed";
+    
+    systemStatus.classList.remove("armed");
+    systemStatus.classList.add("disarmed");
+
+    armButton.classList.remove("armed");
+    armButton.classList.add("disarmed");
   } else if (s === "arm") {
     armButton.textContent = "Disarm";
-    systemStatus.textContent = "Status: Armed";
+    systemStatus.textContent = "Armed";
+    
+    systemStatus.classList.remove("disarmed");
+    systemStatus.classList.add("armed");
+
+    armButton.classList.remove("disarmed");
+    armButton.classList.add("armed");
   }
 }
+
 
 // Toggle the alarm status
 function toggleAlarm() {
   if (s === "arm") {
     client.publish('Status/setStatus', 'disarm');
+    client.publish("status/getStatus", "");
   } else {
     client.publish('Status/setStatus', 'arm');
+    client.publish("status/getStatus", "");
   }
 }
 armButton.addEventListener('click', toggleAlarm);
